752ee4536dacb661e81df7d18d571e05521dc8ac
Sandy Nugraha <sandyan790@gmail.com>
2024-12-16 11:21:29 +0700

    Update Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./utils/theme";
+import { LoginRoute, ProtectedRoute } from "./utils/ProtectedRoute";
 import Home from "./pages/home";
 import Zakat from "./pages/zakat";
 import Artikel from "./pages/artikel";
 import ArtikelPenuh from "./pages/artikel/fullpage";
 import AdminLogin from "./pages/admin/login";
-import { LoginRoute, ProtectedRoute } from "./utils/ProtectedRoute";
-import DashboardLayoutBasic from "./pages/admin";
+import Dashboard from "./pages/admin/dashboard";
+import AdminZakat from "./pages/admin/zakat";
+import AdminLayout from "./pages/admin/AdminLayout";
 
 function App() {
 	return (
@@ -19,10 +21,11 @@ function App() {
 				<Route path='/artikel' element={<Artikel />} />
 				<Route path='/artikel/penuh' element={<ArtikelPenuh />} />
 				<Route element={<LoginRoute />}>
-					<Route path='/admin/login' element={<AdminLogin/>} />
+					<Route path='/admin/login' element={<AdminLogin />} />
 				</Route>
 				<Route element={<ProtectedRoute />}>
-					<Route path='/admin' element={<DashboardLayoutBasic/>} />
+					<Route path='/admin' element={<AdminLayout><Dashboard/></AdminLayout>} />
+					<Route path='/admin/zakat' element={<AdminLayout ><AdminZakat/></AdminLayout>} />
 				</Route>
 			</Routes>
 		</ThemeProvider>
---
+++ src/pages/admin/AdminLayout.jsx
@@ -0,0 +1,92 @@
+// import * as React from 'react'
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import { createTheme } from "@mui/material/styles";
+import { AppProvider } from "@toolpad/core/react-router-dom";
+import { DashboardLayout, ThemeSwitcher } from "@toolpad/core/DashboardLayout";
+import { DashboardOutlined, RedeemOutlined } from "@mui/icons-material";
+import { Button, Stack } from "@mui/material";
+
+const NAVIGATION = [
+	{
+		segment: "admin",
+		title: "Dashboard",
+		icon: <DashboardOutlined />,
+	},
+	{
+		segment: "admin/zakat",
+		title: "Zakat",
+		icon: <RedeemOutlined />,
+	},
+];
+
+const customTheme = createTheme({
+	cssVariables: {
+		colorSchemeSelector: "data-toolpad-color-scheme",
+	},
+	colorSchemes: { light: true, dark: true },
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 600,
+			lg: 1200,
+			xl: 1536,
+		},
+	},
+});
+
+// function CustomAppTitle() {
+// 	return (
+// 		<Typography variant='h5' ml={2} my={0.7} color='white' fontWeight={300}>
+// 			<b>Yuk</b>Zakat.id
+// 		</Typography>
+// 	);
+// }
+
+function Logout() {
+	return (
+		<Stack direction='row' spacing={2}>
+			<Button>Logout</Button>
+			<ThemeSwitcher />
+		</Stack>
+	);
+}
+
+function AdminLayout({ children }) {
+	return (
+		<AppProvider
+			navigation={NAVIGATION}
+			theme={customTheme}
+			branding={{
+				logo: (
+					<Typography
+						variant='h5'
+						ml={2}
+						my={0.7}
+						color='white'
+						fontWeight={300}
+					>
+						<b>Yuk</b>Zakat.id
+					</Typography>
+				),
+				title: "",
+			}}
+		>
+			<DashboardLayout
+				slots={{
+					// appTitle: CustomAppTitle,
+					toolbarActions: Logout,
+				}}
+			>
+				{children}
+			</DashboardLayout>
+		</AppProvider>
+	);
+}
+
+AdminLayout.propTypes = {
+	children: PropTypes.func,
+};
+
+export default AdminLayout;
