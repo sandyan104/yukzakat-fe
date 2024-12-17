366933ab69c7de25a484e806094a3cab4933fca4
ilhamramaw <irama2785@gmail.com>
2024-12-17 13:56:19 +0700

    nambah gambar pembayaran zakat

--- src/pages/artikel/index.jsx
+++ src/pages/artikel/index.jsx
@@ -4,6 +4,7 @@ import {
 	Grid2 as Grid,
 	Typography,
 	Paper,
+	CardActionArea
 } from "@mui/material";
 import styles from "./styles";
 // import { useNavigate } from "react-router-dom";
@@ -19,72 +20,63 @@ const Artikel = () => {
 	}, []);
 
 	return (
-		<>
-			<div style={styles.container}>
-				<Typography variant='h4' mt={4} mb={6} color='white' fontWeight={300}>
-					<b>Yuk</b>Zakat.id
-				</Typography>
-				<Container
-					disableGutters
-					maxWidth='md'
-					component={Paper}
-					sx={{
-						borderRadius: 4,
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						marginBottom: "80px",
-						padding: "40px",
-					}}
-				>
-					<Typography
-						variant='h4'
-						mb={4}
-						color='#4a628a'
-						fontWeight={700}
-						textAlign='center'
-					>
-						Kabar Zakat
-					</Typography>
-					<Grid container spacing={3}>
-						{artikels.map((artikel) => (
-							<Grid item xs={12} md={6} lg={4} key={artikel.id_artikel}>
-								<Paper elevation={3} sx={{ padding: 2 }}>
-									<img
-										src={artikel.hero_image}
-										alt={artikel.judul}
-										style={{ width: "100%", borderRadius: "8px" }}
-									/>
-									<Typography gutterBottom variant='h5' component='div' mt={2}>
-										{artikel.judul}
-									</Typography>
-									<Typography variant='body2' color='text.secondary'>
-										Contact Amil: {artikel.no_telp_amil}
-									</Typography>
-								</Paper>
-							</Grid>
-						))}
-					</Grid>
-				</Container>
-			</div>
+    <>
+      <div style={styles.container}>
+        <Typography variant="h4" mt={4} mb={6} color="white" fontWeight={300}>
+          <b>Yuk</b>Zakat.id
+        </Typography>
+        <Container
+          disableGutters
+          maxWidth="md"
+          component={Paper}
+          sx={{
+            borderRadius: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginBottom: "80px",
+            padding: "40px",
+          }}
+        >
+          <Typography variant="h4" mb={4} color="#4a628a" fontWeight={700} textAlign="center">
+            Kabar Zakat
+          </Typography>
+          <Grid container spacing={3}>
+            <CardActionArea>
+              {artikels.map((artikel) => (
+                <Grid item xs={12} md={6} lg={4} key={artikel.id_artikel}>
+                  <Paper elevation={3} sx={{ padding: 2 }}>
+                    <img src={artikel.hero_image} alt={artikel.judul} style={{ width: "100%", borderRadius: "8px" }} />
+                    <Typography gutterBottom variant="h5" component="div" mt={2}>
+                      {artikel.judul}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Contact Amil: {artikel.no_telp_amil}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              ))}
+            </CardActionArea>
+          </Grid>
+        </Container>
+      </div>
 
-			<div
-				style={{
-					width: "100%",
-					padding: "50px 80px",
-					backgroundColor: "#4A628A",
-				}}
-			>
-				<Typography variant='h5' mb={2} color='white' fontWeight={300}>
-					<b>Yuk</b>Zakat.id
-				</Typography>
-				<Typography variant='body2' color='white' fontWeight={200}>
-					Jl. Raya Katapang-Andir, Sangkanhurip, Katapang, Kab.Bandung Jl. Kebon
-					Bibit Barat, Tamansari, Bandungwetan, Kota Bandung
-				</Typography>
-			</div>
-		</>
-	);
+      <div
+        style={{
+          width: "100%",
+          padding: "50px 80px",
+          backgroundColor: "#4A628A",
+        }}
+      >
+        <Typography variant="h5" mb={2} color="white" fontWeight={300}>
+          <b>Yuk</b>Zakat.id
+        </Typography>
+        <Typography variant="body2" color="white" fontWeight={200}>
+          Jl. Raya Katapang-Andir, Sangkanhurip, Katapang, Kab.Bandung Jl. Kebon Bibit Barat, Tamansari, Bandungwetan, Kota Bandung
+        </Typography>
+      </div>
+    </>
+  );
 };
 
 export default Artikel;
--- src/pages/zakat/index.jsx
+++ src/pages/zakat/index.jsx
@@ -1,48 +1,35 @@
-import {
-	Button,
-	Container,
-	FormControl,
-	FormControlLabel,
-	FormLabel,
-	InputLabel,
-	MenuItem,
-	Paper,
-	Radio,
-	RadioGroup,
-	Select,
-	Stack,
-	TextField,
-	Typography,
-} from "@mui/material";
+import { Button, Container, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Paper, Radio, RadioGroup, Select, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 import styles from "./styles";
 import { useNavigate } from "react-router-dom";
+import niat from "../../assets/niat.png";
+import bayar from "../../assets/bank.png";
 
 const Zakat = () => {
-	const [formData, setFormData] = useState({
-		nama_mz: "",
-		jk_mz: "",
-		no_telp_mz: "",
-		email_mz: "",
-		tgl_Zakat: new Date().toISOString().split("T")[0], // Default to today
-		id_tipe: "",
-		id_rekap: 1, // Replace with your logic for assigning `id_rekap`
-	});
+  const [formData, setFormData] = useState({
+    nama_mz: "",
+    jk_mz: "",
+    no_telp_mz: "",
+    email_mz: "",
+    tgl_Zakat: new Date().toISOString().split("T")[0], // Default to today
+    id_tipe: "",
+    id_rekap: 1, // Replace with your logic for assigning `id_rekap`
+  });
 
-	// Handle form changes
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prev) => ({
-			...prev,
-			[name]: value,
-		}));
-	};
+  // Handle form changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-	const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
-	const navigate = useNavigate();
-	const handleSubmit = async (e) => {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Create FormData
@@ -60,9 +47,9 @@ const Zakat = () => {
       });
       console.log("Success:", response.data);
       alert("Zakat entry created successfully!");
-			setImage(null);
+      setImage(null);
       setPreview(null);
-			setFormData({
+      setFormData({
         nama_mz: "",
         jk_mz: "",
         no_telp_mz: "",
@@ -72,63 +59,59 @@ const Zakat = () => {
         id_rekap: 1,
         nominal: "",
       });
-			
-			navigate("/zakat/buktibayar");
 
-      
+      navigate("/zakat/buktibayar");
     } catch (error) {
       console.error("Error submitting zakat:", error);
       alert("Failed to submit zakat.");
     }
   };
 
-	
+  // Handle file change
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
 
-	// Handle file change
-	const handleFileChange = (e) => {
-		const file = e.target.files[0];
-		if (file) {
-			setImage(file);
+      // Generate preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-			// Generate preview
-			const reader = new FileReader();
-			reader.onloadend = () => {
-				setPreview(reader.result);
-			};
-			reader.readAsDataURL(file);
-		}
-	};
+  // Handle form submission
+  const handleSubmitImage = async (e) => {
+    e.preventDefault();
 
-	// Handle form submission
-	const handleSubmitImage = async (e) => {
-		e.preventDefault();
+    // Create FormData object
+    const data = new FormData();
+    data.append("nama_mz", formData.nama_mz);
+    data.append("jk_mz", formData.jk_mz);
+    data.append("no_telp_mz", formData.no_telp_mz);
+    data.append("email_mz", formData.email_mz);
+    data.append("tgl_Zakat", formData.tgl_Zakat);
+    data.append("id_tipe", formData.id_tipe);
+    data.append("id_rekap", formData.id_rekap);
+    data.append("nominal", formData.nominal);
+    if (image) data.append("hero_image", image);
 
-		// Create FormData object
-		const data = new FormData();
-		data.append("nama_mz", formData.nama_mz);
-		data.append("jk_mz", formData.jk_mz);
-		data.append("no_telp_mz", formData.no_telp_mz);
-		data.append("email_mz", formData.email_mz);
-		data.append("tgl_Zakat", formData.tgl_Zakat);
-		data.append("id_tipe", formData.id_tipe);
-		data.append("id_rekap", formData.id_rekap);
-		data.append("nominal", formData.nominal);
-		if (image) data.append("hero_image", image);
+    try {
+      const response = await axios.post("http://localhost:8000/zakat/", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      alert("Zakat entry created successfully!");
+    } catch (error) {
+      console.error("Error submitting zakat:", error);
+      alert("Failed to submit zakat.");
+    }
+  };
 
-		try {
-			const response = await axios.post("http://localhost:8000/zakat/", data, {
-				headers: {
-					"Content-Type": "multipart/form-data",
-				},
-			});
-			alert("Zakat entry created successfully!");
-		} catch (error) {
-			console.error("Error submitting zakat:", error);
-			alert("Failed to submit zakat.");
-		}
-	};
-
-	return (
+  return (
     <>
       <div style={styles.container}>
         <Typography variant="h4" mt={4} mb={6} color="white" fontWeight={300}>
@@ -200,6 +183,7 @@ const Zakat = () => {
           <Typography variant="h4" mb={4} color="#4a628a" fontWeight={700} textAlign="center" sx={{ marginBottom: "45px" }}>
             Pembayaran
           </Typography>
+          <img src={bayar} width="70%" style={{ marginBottom: "50px" }} />
           <Stack direction="row" spacing={2} sx={{ width: "100%" }} mb={3}>
             <TextField
               label="Bukti Pembayaran"
@@ -220,7 +204,8 @@ const Zakat = () => {
           </Stack>
           {preview && <img src={preview} alt="Preview" width="200" />}
 
-          <Typography sx={{fontSize: "10px", paddingBottom: "10px", textAlign: "justify", paddingTop:"10px"}}>
+          <img src={niat} width="80%" style={{ margin: "20px" }} />
+          <Typography sx={{ fontSize: "10px", paddingBottom: "10px", textAlign: "justify", paddingTop: "10px" }}>
             Dengan mengisi formulir ini, Muzaki menyatakan bahwa zakat yang ditunaikan berasal dari sumber halal yang sesuai dengan peraturan, bukan hasil pencucian uang, dan merupakan harta yang
             dimiliki secara penuh, serta menyetujui untuk menerima Bukti Setor Zakat (BSZ), laporan penyaluran, dan informasi layanan BAZNAS melalui email dan WhatsApp
           </Typography>
